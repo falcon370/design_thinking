27a6eb8ceff03fc860eb566bebc478bd711579a1
Pranav Napolean <ppnapolean@gmail.com>
2026-01-04 16:45:16 +0530

    project cleanup

--- crowd_monitoring/debug_mp_v2.py
+++
@@ -1,8 +0,0 @@
-import cv2
-try:
-    import mediapipe.tasks
-    print("mediapipe.tasks imported")
-except ImportError:
-    print("mediapipe.tasks failed")
-
-print(f"OpenCV Version: {cv2.__version__}")
